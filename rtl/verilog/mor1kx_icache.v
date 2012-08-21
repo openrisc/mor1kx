ff6e01da2770959f9e0e13f65aae80dcbf874ab3
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2012-08-21 22:16:26 +0300

    Add OHDL headers

--- rtl/verilog/mor1kx_icache.v
+++ rtl/verilog/mor1kx_icache.v
@@ -1,6 +1,15 @@
-/*
- * (C) Copyright 2011, Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
- */
+/******************************************************************************
+ This Source Code Form is subject to the terms of the
+ Open Hardware Description License, v. 1.0. If a copy
+ of the OHDL was not distributed with this file, You
+ can obtain one at http://juliusbaxter.net/ohdl/ohdl.txt
+
+ Description: Instruction cache implementation
+
+ Copyright (C) 2012 Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
+
+ ******************************************************************************/
+
 `include "mor1kx-defines.v"
 
 module mor1kx_icache
