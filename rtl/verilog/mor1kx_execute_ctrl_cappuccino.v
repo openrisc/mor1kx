ecb93e7ea370943568ff77c80cd087ce327e1c9f
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2012-12-03 06:41:20 +0200

    cappuccino/execute_ctrl: whitespace cleanup

--- rtl/verilog/mor1kx_execute_ctrl_cappuccino.v
+++ rtl/verilog/mor1kx_execute_ctrl_cappuccino.v
@@ -1,17 +1,17 @@
 /* ****************************************************************************
-  This Source Code Form is subject to the terms of the 
-  Open Hardware Description License, v. 1.0. If a copy 
-  of the OHDL was not distributed with this file, You 
+  This Source Code Form is subject to the terms of the
+  Open Hardware Description License, v. 1.0. If a copy
+  of the OHDL was not distributed with this file, You
   can obtain one at http://juliusbaxter.net/ohdl/ohdl.txt
 
   Description: execute stage control
-   
+
   Generate valid signal when stage is done
- 
+
   Copyright (C) 2012 Authors
- 
+
   Author(s): Julius Baxter <juliusbaxter@gmail.com>
- 
+
 ***************************************************************************** */
 
 `include "mor1kx-defines.v"
@@ -26,18 +26,18 @@ module mor1kx_execute_ctrl_cappuccino
    opc_insn_i,
    // PC of execute stage instruction
    pc_execute_i,
-   
-   execute_except_ibus_err_i, execute_except_illegal_i, 
+
+   execute_except_ibus_err_i, execute_except_illegal_i,
    execute_except_syscall_i, execute_except_trap_i,
    lsu_except_dbus_i, lsu_except_align_i,
 
    pipeline_flush_i,
-   
+
 
    // Whether this operation relies on ALU output or not
    op_alu_i,
-   
-   op_lsu_load_i, op_lsu_store_i,   
+
+   op_lsu_load_i, op_lsu_store_i,
 
    alu_valid_i, lsu_valid_i,
 
@@ -59,29 +59,29 @@ module mor1kx_execute_ctrl_cappuccino
    pc_ctrl_o,
    ctrl_opc_insn_o,
 
-   ctrl_except_ibus_err_o, ctrl_except_ibus_align_o, ctrl_except_illegal_o, 
-   ctrl_except_syscall_o, ctrl_except_dbus_o, ctrl_except_align_o, 
+   ctrl_except_ibus_err_o, ctrl_except_ibus_align_o, ctrl_except_illegal_o,
+   ctrl_except_syscall_o, ctrl_except_dbus_o, ctrl_except_align_o,
    ctrl_except_trap_o,
 
    execute_waiting_o,
-   
+
    execute_valid_o
    );
 
    parameter OPTION_OPERAND_WIDTH = 32;
    parameter OPTION_RESET_PC = {{(OPTION_OPERAND_WIDTH-13){1'b0}},
 				`OR1K_RESET_VECTOR,8'd0};
-   
+
    input clk, rst;
-   
+
    input padv_i;
 
    // insn opcode, indicating what's going on
    input [`OR1K_OPCODE_WIDTH-1:0] opc_insn_i;
-   
+
    input 			  execute_except_ibus_err_i,
-				  execute_except_illegal_i, 
-				  execute_except_syscall_i, lsu_except_dbus_i, 
+				  execute_except_illegal_i,
+				  execute_except_syscall_i, lsu_except_dbus_i,
 				  lsu_except_align_i, execute_except_trap_i;
 
    input 			  pipeline_flush_i;
@@ -93,39 +93,39 @@ module mor1kx_execute_ctrl_cappuccino
    input 			  alu_valid_i, lsu_valid_i;
 
    input 			  op_jr_i;
-   
+
    input [OPTION_OPERAND_WIDTH-1:0] alu_result_i;
    input [OPTION_OPERAND_WIDTH-1:0] rfb_i;
    input 			     flag_set_i, flag_clear_i;
    input [OPTION_OPERAND_WIDTH-1:0] pc_execute_i;
 
    input 			     rf_wb_i;
-   
+
    // Input from control stage for mfspr WE
    input 			     ctrl_mfspr_we_i;
-   
-   
+
+
    output 			     execute_rf_we_o;
 
    // Combinatorial output of instruction fetch align error
    output 			     execute_except_ibus_align_o;
-   
+
    output reg [OPTION_OPERAND_WIDTH-1:0] ctrl_alu_result_o;
    output reg [OPTION_OPERAND_WIDTH-1:0] ctrl_rfb_o;
    output reg 				  ctrl_flag_set_o, ctrl_flag_clear_o;
    output reg [OPTION_OPERAND_WIDTH-1:0] pc_ctrl_o;
    output reg [`OR1K_OPCODE_WIDTH-1:0] 	  ctrl_opc_insn_o;
-   
+
    output reg 				  ctrl_except_ibus_err_o,
-					  ctrl_except_ibus_align_o, 
-					  ctrl_except_illegal_o, 
-					  ctrl_except_syscall_o, 
-					  ctrl_except_dbus_o, 
-					  ctrl_except_align_o, 
+					  ctrl_except_ibus_align_o,
+					  ctrl_except_illegal_o,
+					  ctrl_except_syscall_o,
+					  ctrl_except_dbus_o,
+					  ctrl_except_align_o,
 					  ctrl_except_trap_o;
 
    output reg 				  execute_waiting_o;
-   
+
    output reg 				  execute_valid_o;
 
    wire 				  execute_valid;
@@ -133,7 +133,7 @@ module mor1kx_execute_ctrl_cappuccino
    wire 				  op_mfspr;
 
    assign op_mfspr = opc_insn_i==`OR1K_OPCODE_MFSPR;
-   
+
    // ALU or LSU stall execution, nothing else can
    assign execute_valid = (op_alu_i) ? alu_valid_i :
 			  (op_lsu_load_i | op_lsu_store_i) ? lsu_valid_i :
@@ -141,7 +141,7 @@ module mor1kx_execute_ctrl_cappuccino
 
    // Do writeback when we register our output to the next stage, or if
    // we're doing mfspr
-   assign execute_rf_we_o = (rf_wb_i & padv_i & !op_mfspr 
+   assign execute_rf_we_o = (rf_wb_i & padv_i & !op_mfspr
 			     & !((op_lsu_load_i | op_lsu_store_i) &
 				 lsu_except_dbus_i | lsu_except_align_i)) |
 			    (op_mfspr & ctrl_mfspr_we_i);
@@ -155,7 +155,7 @@ module mor1kx_execute_ctrl_cappuccino
 	execute_waiting_o = !execute_valid;
      end
    /*
-  
+
    always @(posedge clk `OR_ASYNC_RST)
      if (rst)
        execute_valid_o <= 0;
@@ -167,12 +167,12 @@ module mor1kx_execute_ctrl_cappuccino
        execute_waiting_o <= 0;
      else if (execute_valid)
        execute_waiting_o <= 0;
-     else 
+     else
        execute_waiting_o <= (op_alu_i & !alu_valid_i) |
-			    ((op_lsu_load_i | op_lsu_store_i) & 
+			    ((op_lsu_load_i | op_lsu_store_i) &
 			     !lsu_valid_i) & !padv_i;
     */
-			 
+
    always @(posedge clk `OR_ASYNC_RST)
      if (rst) begin
 	ctrl_except_ibus_err_o <= 0;
@@ -209,7 +209,7 @@ module mor1kx_execute_ctrl_cappuccino
 	ctrl_alu_result_o <= 0;
      else if (padv_i)
        ctrl_alu_result_o <= alu_result_i;
-   
+
    always @(posedge clk `OR_ASYNC_RST)
      if (rst)
        ctrl_rfb_o <= 0;
@@ -227,7 +227,7 @@ module mor1kx_execute_ctrl_cappuccino
 	ctrl_flag_set_o <= flag_set_i;
 	ctrl_flag_clear_o <= flag_clear_i;
      end
-   
+
    always @(posedge clk `OR_ASYNC_RST)
      if (rst)
        pc_ctrl_o <= OPTION_RESET_PC;
@@ -241,8 +241,5 @@ module mor1kx_execute_ctrl_cappuccino
        ctrl_opc_insn_o <= `OR1K_OPCODE_NOP;
      else if (padv_i)
        ctrl_opc_insn_o <= opc_insn_i;
-   
+
 endmodule // mor1kx_execute_ctrl_cappuccino
-
-
-   
