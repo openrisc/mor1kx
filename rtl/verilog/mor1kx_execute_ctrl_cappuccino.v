d5a8334c5e3bbab271fb5a01951ac912a63df1a9
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2012-12-03 06:44:16 +0200

    cappuccino/execute_ctrl: verilog 2001 style module args

--- rtl/verilog/mor1kx_execute_ctrl_cappuccino.v
+++ rtl/verilog/mor1kx_execute_ctrl_cappuccino.v
@@ -17,116 +17,74 @@
 `include "mor1kx-defines.v"
 
 module mor1kx_execute_ctrl_cappuccino
-  (
-   clk, rst,
+  #(
+    parameter OPTION_OPERAND_WIDTH = 32,
+    parameter OPTION_RESET_PC = {{(OPTION_OPERAND_WIDTH-13){1'b0}},
+				 `OR1K_RESET_VECTOR,8'd0}
+    )
+   (
+    input 				  clk,
+    input 				  rst,
 
-   padv_i,
+    input 				  padv_i,
 
-   // insn opcode, indicating what's going on
-   opc_insn_i,
-   // PC of execute stage instruction
-   pc_execute_i,
+    // insn opcode, indicating what's going on
+    input [`OR1K_OPCODE_WIDTH-1:0] 	  opc_insn_i,
 
-   execute_except_ibus_err_i, execute_except_illegal_i,
-   execute_except_syscall_i, execute_except_trap_i,
-   lsu_except_dbus_i, lsu_except_align_i,
+    input 				  execute_except_ibus_err_i,
+    input 				  execute_except_illegal_i,
+    input 				  execute_except_syscall_i,
+    input 				  lsu_except_dbus_i,
+    input 				  lsu_except_align_i,
+    input 				  execute_except_trap_i,
 
-   pipeline_flush_i,
+    input 				  pipeline_flush_i,
 
+    input 				  op_alu_i,
+    input 				  op_lsu_load_i,
+    input 				  op_lsu_store_i,
 
-   // Whether this operation relies on ALU output or not
-   op_alu_i,
+    input 				  alu_valid_i,
+    input 				  lsu_valid_i,
 
-   op_lsu_load_i, op_lsu_store_i,
+    input 				  op_jr_i,
 
-   alu_valid_i, lsu_valid_i,
+    input [OPTION_OPERAND_WIDTH-1:0] 	  alu_result_i,
+    input [OPTION_OPERAND_WIDTH-1:0] 	  rfb_i,
+    input 				  flag_set_i,
+    input 				  flag_clear_i,
+    input [OPTION_OPERAND_WIDTH-1:0] 	  pc_execute_i,
 
-   op_jr_i,
+    input 				  rf_wb_i,
 
-   // Inputs that we'll want in control/SPR stage
-   alu_result_i, rfb_i,
-   flag_set_i, flag_clear_i,
-
-   ctrl_mfspr_we_i,
-
-   rf_wb_i,
-   execute_rf_we_o,
-
-   execute_except_ibus_align_o,
-
-   ctrl_alu_result_o, ctrl_rfb_o,
-   ctrl_flag_set_o, ctrl_flag_clear_o,
-   pc_ctrl_o,
-   ctrl_opc_insn_o,
-
-   ctrl_except_ibus_err_o, ctrl_except_ibus_align_o, ctrl_except_illegal_o,
-   ctrl_except_syscall_o, ctrl_except_dbus_o, ctrl_except_align_o,
-   ctrl_except_trap_o,
-
-   execute_waiting_o,
-
-   execute_valid_o
-   );
-
-   parameter OPTION_OPERAND_WIDTH = 32;
-   parameter OPTION_RESET_PC = {{(OPTION_OPERAND_WIDTH-13){1'b0}},
-				`OR1K_RESET_VECTOR,8'd0};
-
-   input clk, rst;
-
-   input padv_i;
-
-   // insn opcode, indicating what's going on
-   input [`OR1K_OPCODE_WIDTH-1:0] opc_insn_i;
-
-   input 			  execute_except_ibus_err_i,
-				  execute_except_illegal_i,
-				  execute_except_syscall_i, lsu_except_dbus_i,
-				  lsu_except_align_i, execute_except_trap_i;
-
-   input 			  pipeline_flush_i;
-
-   input 			  op_alu_i;
-   input 			  op_lsu_load_i;
-   input 			  op_lsu_store_i;
-
-   input 			  alu_valid_i, lsu_valid_i;
-
-   input 			  op_jr_i;
-
-   input [OPTION_OPERAND_WIDTH-1:0] alu_result_i;
-   input [OPTION_OPERAND_WIDTH-1:0] rfb_i;
-   input 			     flag_set_i, flag_clear_i;
-   input [OPTION_OPERAND_WIDTH-1:0] pc_execute_i;
-
-   input 			     rf_wb_i;
-
-   // Input from control stage for mfspr WE
-   input 			     ctrl_mfspr_we_i;
+    // Input from control stage for mfspr WE
+    input 				  ctrl_mfspr_we_i,
 
 
-   output 			     execute_rf_we_o;
+    output 				  execute_rf_we_o,
 
-   // Combinatorial output of instruction fetch align error
-   output 			     execute_except_ibus_align_o;
+    // Combinatorial output of instruction fetch align error
+    output 				  execute_except_ibus_align_o,
 
-   output reg [OPTION_OPERAND_WIDTH-1:0] ctrl_alu_result_o;
-   output reg [OPTION_OPERAND_WIDTH-1:0] ctrl_rfb_o;
-   output reg 				  ctrl_flag_set_o, ctrl_flag_clear_o;
-   output reg [OPTION_OPERAND_WIDTH-1:0] pc_ctrl_o;
-   output reg [`OR1K_OPCODE_WIDTH-1:0] 	  ctrl_opc_insn_o;
+    output reg [OPTION_OPERAND_WIDTH-1:0] ctrl_alu_result_o,
+    output reg [OPTION_OPERAND_WIDTH-1:0] ctrl_rfb_o,
+    output reg 				  ctrl_flag_set_o,
+    output reg 				  ctrl_flag_clear_o,
+    output reg [OPTION_OPERAND_WIDTH-1:0] pc_ctrl_o,
+    output reg [`OR1K_OPCODE_WIDTH-1:0]   ctrl_opc_insn_o,
 
-   output reg 				  ctrl_except_ibus_err_o,
-					  ctrl_except_ibus_align_o,
-					  ctrl_except_illegal_o,
-					  ctrl_except_syscall_o,
-					  ctrl_except_dbus_o,
-					  ctrl_except_align_o,
-					  ctrl_except_trap_o;
+    output reg 				  ctrl_except_ibus_err_o,
+    output reg 				  ctrl_except_ibus_align_o,
+    output reg 				  ctrl_except_illegal_o,
+    output reg 				  ctrl_except_syscall_o,
+    output reg 				  ctrl_except_dbus_o,
+    output reg 				  ctrl_except_align_o,
+    output reg 				  ctrl_except_trap_o,
 
-   output reg 				  execute_waiting_o;
+    output reg 				  execute_waiting_o,
 
-   output reg 				  execute_valid_o;
+    output reg 				  execute_valid_o
+    );
 
    wire 				  execute_valid;
 
