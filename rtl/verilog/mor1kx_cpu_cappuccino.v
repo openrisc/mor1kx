5e1f17403a228a149d0379c9c8a3412851fc71d7
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2012-12-04 06:46:24 +0200

    cappuccino/cpu: connect lsu_adr_i to adder_result_o

--- rtl/verilog/mor1kx_cpu_cappuccino.v
+++ rtl/verilog/mor1kx_cpu_cappuccino.v
@@ -448,7 +448,7 @@ module mor1kx_cpu_cappuccino
    /* mor1kx_lsu_cappuccino AUTO_TEMPLATE (
     .padv_execute_i			(padv_execute_o),
     .decode_valid_i			(decode_valid_o),
-    .lsu_adr_i				(alu_result_o),
+    .lsu_adr_i				(adder_result_o),
     .rfb_i				(rfb_o),
     .opc_insn_i			        (opc_insn_o),
     .op_lsu_load_i			(op_lsu_load_o),
@@ -476,7 +476,7 @@ module mor1kx_cpu_cappuccino
       .rst				(rst),
       .padv_execute_i			(padv_execute_o),	 // Templated
       .decode_valid_i			(decode_valid_o),	 // Templated
-      .lsu_adr_i			(alu_result_o),		 // Templated
+      .lsu_adr_i			(adder_result_o),	 // Templated
       .rfb_i				(rfb_o),		 // Templated
       .opc_insn_i			(opc_insn_o),		 // Templated
       .op_lsu_load_i			(op_lsu_load_o),	 // Templated
