4d8b27fab82d2913c8241d8aa578dd7d6fde8d57
Julius Baxter <juliusbaxter@gmail.com>
2012-12-16 19:16:13 +0000

    mor1kx-sprs: bring up to date with OR1K 1.0

--- rtl/verilog/mor1kx-sprs.v
+++ rtl/verilog/mor1kx-sprs.v
@@ -24,15 +24,20 @@
 `define OR1K_SPR_ICCFGR_ADDR    {5'd0,11'd6}
 `define OR1K_SPR_DCFGR_ADDR     {5'd0,11'd7}
 `define OR1K_SPR_PCCFGR_ADDR    {5'd0,11'd8}
+`define OR1K_SPR_VR2_ADDR       {5'd0,11'd9}
+`define OR1K_SPR_AVR_ADDR       {5'd0,11'd10}
+`define OR1K_SPR_EVBAR_ADDR     {5'd0,11'd11}
+`define OR1K_SPR_AECR_ADDR      {5'd0,11'd12}
+`define OR1K_SPR_AESR_ADDR      {5'd0,11'd13}
 `define OR1K_SPR_NPC_ADDR       {5'd0,11'd16}
 `define OR1K_SPR_SR_ADDR        {5'd0,11'd17}
 `define OR1K_SPR_PPC_ADDR       {5'd0,11'd18}
 `define OR1K_SPR_FPCSR_ADDR     {5'd0,11'd20}
+`define OR1K_SPR_ISR0_ADDR      {5'd0,11'd21}
 `define OR1K_SPR_EPCR0_ADDR     {5'd0,11'd32}
 `define OR1K_SPR_EEAR0_ADDR     {5'd0,11'd48}
 `define OR1K_SPR_ESR0_ADDR      {5'd0,11'd64}
 `define OR1K_SPR_GPR0_ADDR      {5'd0,11'd1024}
-`define OR1K_SPR_EVBA_ADDR      {5'd0,11'd1536}
 `define OR1K_SPR_DMMUCR_ADDR    {5'd1,11'd0}
 `define OR1K_SPR_DMMUPR_ADDR    {5'd1,11'd1}
 `define OR1K_SPR_DTLBEIR_ADDR   {5'd1,11'd2}
@@ -115,9 +120,10 @@
 `define OR1K_SPR_SR_RESERVED 27:17 /* Reserved */
 `define OR1K_SPR_SR_CID      31:28 /* Context ID */
 
-// Version register
+// Version register - DEPRECATED
 `define OR1K_SPR_VR_REV   5:0  /* Revision */
-`define OR1K_SPR_VR_RESERVED 15:6 /* Reserved */
+`define OR1K_SPR_VR_UVRP  6  /* Updated Version Registers Present */
+`define OR1K_SPR_VR_RESERVED 15:7 /* Reserved */
 `define OR1K_SPR_VR_CFG   23:16  /* Configuration Template */
 `define OR1K_SPR_VR_VER   31:24  /* Version */
 
@@ -145,8 +151,46 @@
 `define OR1K_SPR_CPUCFGR_OF32S  7
 `define OR1K_SPR_CPUCFGR_OF64S  8
 `define OR1K_SPR_CPUCFGR_OV64S  9
-`define OR1K_SPR_CPUCFGR_RESERVED 31:10
+`define OR1K_SPR_CPUCFGR_ND     10 /* No delay-slot implementation */
+`define OR1K_SPR_CPUCFGR_AVRP   11 /* Arch. version registers */
+`define OR1K_SPR_CPUCFGR_EVBARP 12 /* Exception vector base addr reg */
+`define OR1K_SPR_CPUCFGR_ISRP   13 /* Implementation specific regs */
+`define OR1K_SPR_CPUCFGR_AECSRP 14 /* Arith. exception regs */
+`define OR1K_SPR_CPUCFGR_RESERVED 31:15
 
+// Version register 2 (new with OR1K 1.0)
+`define OR1K_SPR_VR2_VER 23:0
+`define OR1K_SPR_VR2_CPUID 31:24
+
+// Architecture Version register
+`define OR1K_SPR_AVR_RESERVED 7:0
+`define OR1K_SPR_AVR_REV 15:8
+`define OR1K_SPR_AVR_MIN 23:16
+`define OR1K_SPR_AVR_MAJ 31:24
+
+// Exception Vector Base Address register
+`define OR1K_SPR_EVBAR_RESERVED 12:0
+`define OR1K_SPR_EVBAR_EVBA 31:13
+
+// Arithmetic Exception Control register
+`define OR1K_SPR_AECR_CYADDE    0
+`define OR1K_SPR_AECR_OVADDE    1
+`define OR1K_SPR_AECR_CYMULE    2
+`define OR1K_SPR_AECR_OVMULE    3
+`define OR1K_SPR_AECR_DBZE      4
+`define OR1K_SPR_AECR_CYMACADDE 5
+`define OR1K_SPR_AECR_OVMACADDE 6
+`define OR1K_SPR_AECR_RESERVED  31:7
+
+// Arithmetic Exception Status register
+`define OR1K_SPR_AESR_CYADDE    0
+`define OR1K_SPR_AESR_OVADDE    1
+`define OR1K_SPR_AESR_CYMULE    2
+`define OR1K_SPR_AESR_OVMULE    3
+`define OR1K_SPR_AESR_DBZE      4
+`define OR1K_SPR_AESR_CYMACADDE 5
+`define OR1K_SPR_AESR_OVMACADDE 6
+`define OR1K_SPR_AESR_RESERVED  31:7
 
 // Tick timer registers
 `define OR1K_SPR_TTMR_TP   27:0 /* Time period */
