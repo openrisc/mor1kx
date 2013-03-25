1f5b6c36ff246fc61069df18884e26efb045214e
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2013-03-25 10:07:33 +0200

    dcache: fix verilator WIDTH warning on start_adr

--- rtl/verilog/mor1kx_dcache.v
+++ rtl/verilog/mor1kx_dcache.v
@@ -253,7 +253,7 @@ module mor1kx_dcache
 	 // Load address to invalidate from SPR bus
 	 mem_adr <= spr_bus_dat_i;
       end else if (idle & !invalidating) begin
-	 start_adr <= {cpu_adr_i[OPTION_OPERAND_WIDTH-1:2], 2'b0};
+	 start_adr <= {cpu_adr_i[OPTION_DCACHE_BLOCK_WIDTH-1:2], 2'b0};
 	 mem_adr <= {cpu_adr_i[OPTION_OPERAND_WIDTH-1:2], 2'b0};
 	 cpu_bsel_r <= cpu_bsel_i;
 	 /*
