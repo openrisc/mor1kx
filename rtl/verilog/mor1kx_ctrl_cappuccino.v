703b7de7e71a2ffd4eec208f4289980a51de97b4
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2013-04-11 06:00:17 +0300

    cappuccino/ctrl: whitespace fix

--- rtl/verilog/mor1kx_ctrl_cappuccino.v
+++ rtl/verilog/mor1kx_ctrl_cappuccino.v
@@ -573,7 +573,7 @@ module mor1kx_ctrl_cappuccino
 	       spr_sr[`OR1K_SPR_SR_SM  ] <= spr_write_dat[`OR1K_SPR_SR_SM  ];
 
 	       spr_sr[`OR1K_SPR_SR_F  ] <= spr_write_dat[`OR1K_SPR_SR_F  ];
-	       
+
 	       if (FEATURE_TIMER!="NONE")
 		 spr_sr[`OR1K_SPR_SR_TEE ] <= spr_write_dat[`OR1K_SPR_SR_TEE ];
 
