776ca338686150b3f487de3b02b0c84e8e5fe8fa
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2013-07-26 21:01:51 +0300

    dmmu: line up input/output port declarations
    No functional change.

--- rtl/verilog/mor1kx_dmmu.v
+++ rtl/verilog/mor1kx_dmmu.v
@@ -19,29 +19,29 @@ module mor1kx_dmmu
     parameter OPTION_DMMU_WAYS = 1
     )
    (
-    input 			      clk,
-    input 			      rst,
+    input 				  clk,
+    input 				  rst,
 
-    input [OPTION_OPERAND_WIDTH-1:0]  virt_addr_i,
-    input [OPTION_OPERAND_WIDTH-1:0]  virt_addr_match_i,
-    output [OPTION_OPERAND_WIDTH-1:0] phys_addr_o,
-    output 			      cache_inhibit_o,
+    input [OPTION_OPERAND_WIDTH-1:0] 	  virt_addr_i,
+    input [OPTION_OPERAND_WIDTH-1:0] 	  virt_addr_match_i,
+    output [OPTION_OPERAND_WIDTH-1:0] 	  phys_addr_o,
+    output 				  cache_inhibit_o,
 
-    input 			      op_store_i,
-    input 			      op_load_i,
-    input 			      supervisor_mode_i,
+    input 				  op_store_i,
+    input 				  op_load_i,
+    input 				  supervisor_mode_i,
 
-    output 			      tlb_miss_o,
-    output 			      pagefault_o,
+    output 				  tlb_miss_o,
+    output 				  pagefault_o,
 
     // SPR interface
-    input [15:0] 		      spr_bus_addr_i,
-    input 			      spr_bus_we_i,
-    input 			      spr_bus_stb_i,
-    input [OPTION_OPERAND_WIDTH-1:0]  spr_bus_dat_i,
+    input [15:0] 			  spr_bus_addr_i,
+    input 				  spr_bus_we_i,
+    input 				  spr_bus_stb_i,
+    input [OPTION_OPERAND_WIDTH-1:0] 	  spr_bus_dat_i,
 
-    output [OPTION_OPERAND_WIDTH-1:0] spr_bus_dat_o,
-    output 			      spr_bus_ack_o
+    output [OPTION_OPERAND_WIDTH-1:0] 	  spr_bus_dat_o,
+    output 				  spr_bus_ack_o
     );
 
    wire [OPTION_OPERAND_WIDTH-1:0]    dtlb_match_dout;
