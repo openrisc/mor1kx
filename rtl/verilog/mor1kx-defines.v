b5ca2ea80854b92d47aadf508f357b3b6460f23d
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2013-09-01 23:39:55 +0300

    mor1kx v1.0

--- rtl/verilog/mor1kx-defines.v
+++ rtl/verilog/mor1kx-defines.v
@@ -226,8 +226,8 @@
 // mor1kx breaks up the VR2 version register to be 3 8-bit fields
 // MSB is major version, middle byte is minor version number
 // and final byte is the pipeline identifier.
-`define MOR1KX_VERSION_MAJOR 8'd0
-`define MOR1KX_VERSION_MINOR 8'd1
+`define MOR1KX_VERSION_MAJOR 8'd1
+`define MOR1KX_VERSION_MINOR 8'd0
 
 // mor1kx implementation-specific register definitions
 `define MOR1KX_PIPEID_CAPPUCCINO 8'd1
