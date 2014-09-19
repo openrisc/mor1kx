64651c8af488a498f059c54fcd9580b1d16ac6c4
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2014-09-19 13:53:47 +0300

    cappuccino/ctrl: negate external spr units on non access
    This ensures that the external spr units can't assert their
    acks when they are not accessed (this logic has changed,
    earlier they've been allowed to constantly assert ack).
    
    This together with the data negation should be moved to respective
    spr units at some point.

--- rtl/verilog/mor1kx_ctrl_cappuccino.v
+++ rtl/verilog/mor1kx_ctrl_cappuccino.v
@@ -1301,7 +1301,8 @@ module mor1kx_ctrl_cappuccino
 // Controls to generate ACKs from units that are external to this module
 generate
 if (FEATURE_DMMU!="NONE") begin : dmmu_ctrl
-   assign spr_access_ack[`OR1K_SPR_DMMU_BASE] = spr_bus_ack_dmmu_i;
+   assign spr_access_ack[`OR1K_SPR_DMMU_BASE] = spr_bus_ack_dmmu_i &
+                                                spr_access[`OR1K_SPR_DMMU_BASE];
    assign spr_internal_read_dat[`OR1K_SPR_DMMU_BASE] =
      spr_bus_dat_dmmu_i &
      {OPTION_OPERAND_WIDTH{spr_access[`OR1K_SPR_DMMU_BASE]}};
@@ -1313,7 +1314,8 @@ endgenerate
 
 generate
 if (FEATURE_IMMU!="NONE") begin : immu_ctrl
-   assign spr_access_ack[`OR1K_SPR_IMMU_BASE] = spr_bus_ack_immu_i;
+   assign spr_access_ack[`OR1K_SPR_IMMU_BASE] = spr_bus_ack_immu_i &
+                                                spr_access[`OR1K_SPR_IMMU_BASE];
    assign spr_internal_read_dat[`OR1K_SPR_IMMU_BASE] =
      spr_bus_dat_immu_i &
      {OPTION_OPERAND_WIDTH{spr_access[`OR1K_SPR_IMMU_BASE]}};
@@ -1325,7 +1327,8 @@ endgenerate
 
 generate
 if (FEATURE_DATACACHE!="NONE") begin : datacache_ctrl
-   assign spr_access_ack[`OR1K_SPR_DC_BASE] = spr_bus_ack_dc_i;
+   assign spr_access_ack[`OR1K_SPR_DC_BASE] = spr_bus_ack_dc_i &
+                                              spr_access[`OR1K_SPR_DC_BASE];
    assign spr_internal_read_dat[`OR1K_SPR_DC_BASE] =
      spr_bus_dat_dc_i & {OPTION_OPERAND_WIDTH{spr_access[`OR1K_SPR_DC_BASE]}};
 end else begin
@@ -1336,7 +1339,8 @@ endgenerate
 
 generate
 if (FEATURE_INSTRUCTIONCACHE!="NONE") begin : instructioncache_ctrl
-   assign spr_access_ack[`OR1K_SPR_IC_BASE] = spr_bus_ack_ic_i;
+   assign spr_access_ack[`OR1K_SPR_IC_BASE] = spr_bus_ack_ic_i &
+                                              spr_access[`OR1K_SPR_IC_BASE];
    assign spr_internal_read_dat[`OR1K_SPR_IC_BASE] =
      spr_bus_dat_ic_i & {OPTION_OPERAND_WIDTH{spr_access[`OR1K_SPR_IC_BASE]}};
 end else begin
@@ -1347,7 +1351,8 @@ endgenerate
 
 generate
 if (FEATURE_MAC!="NONE") begin : mac_ctrl
-   assign spr_access_ack[`OR1K_SPR_MAC_BASE] = spr_bus_ack_mac_i;
+   assign spr_access_ack[`OR1K_SPR_MAC_BASE] = spr_bus_ack_mac_i &
+                                               spr_access[`OR1K_SPR_MAC_BASE];
    assign spr_internal_read_dat[`OR1K_SPR_MAC_BASE] =
       spr_bus_dat_mac_i &
       {OPTION_OPERAND_WIDTH{spr_access[`OR1K_SPR_MAC_BASE]}};
@@ -1359,7 +1364,8 @@ endgenerate
 
 generate
 if (FEATURE_PERFCOUNTERS!="NONE") begin : perfcounters_ctrl
-   assign spr_access_ack[`OR1K_SPR_PC_BASE] = spr_bus_ack_pcu_i;
+   assign spr_access_ack[`OR1K_SPR_PC_BASE] = spr_bus_ack_pcu_i &
+                                              spr_access[`OR1K_SPR_PC_BASE];
    assign spr_internal_read_dat[`OR1K_SPR_PC_BASE] =
      spr_bus_dat_pcu_i & {OPTION_OPERAND_WIDTH{spr_access[`OR1K_SPR_PC_BASE]}};
 end else begin
@@ -1370,7 +1376,8 @@ endgenerate
 
 generate
 if (FEATURE_PMU!="NONE") begin : pmu_ctrl
-   assign spr_access_ack[`OR1K_SPR_PM_BASE] = spr_bus_ack_pmu_i;
+   assign spr_access_ack[`OR1K_SPR_PM_BASE] = spr_bus_ack_pmu_i &
+                                              spr_access[`OR1K_SPR_PM_BASE];
    assign spr_internal_read_dat[`OR1K_SPR_PM_BASE] =
      spr_bus_dat_pmu_i & {OPTION_OPERAND_WIDTH{spr_access[`OR1K_SPR_PM_BASE]}};
 end else begin
