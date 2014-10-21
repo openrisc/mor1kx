4763ba6bbdb46c0b4c30c850dfcf38f9f11bab70
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2014-10-21 13:50:19 +0200

    Set SPR data read to 0 if DU not present

--- rtl/verilog/mor1kx_ctrl_cappuccino.v
+++ rtl/verilog/mor1kx_ctrl_cappuccino.v
@@ -1285,7 +1285,7 @@ module mor1kx_ctrl_cappuccino
 	   assign du_dat_o = 0;
 	   assign du_restart_from_stall = 0;
            assign spr_access_ack[`OR1K_SPR_DU_BASE] = 1;
-
+           assign spr_internal_read_dat[`OR1K_SPR_DU_BASE] = 0;
 	   always @(posedge clk)
 	     begin
 		spr_dmr1 <= 0;
