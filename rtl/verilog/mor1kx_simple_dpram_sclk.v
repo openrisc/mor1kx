44b5095f5ca919f295604b759c11fbd88469caad
Olof Kindgren <olof.kindgren@gmail.com>
2015-02-04 07:59:16 +0100

    Add option to clear RF in simulations
    When building against FPGA targets, we can often assume that internal
    memories are cleared after power-on and some (buggy) software relies
    on that. This adds a parameter to initialize the RF to zero for
    RTL simulations.
    
    Signed-off-by: Olof Kindgren <olof.kindgren@gmail.com>

--- rtl/verilog/mor1kx_simple_dpram_sclk.v
+++ rtl/verilog/mor1kx_simple_dpram_sclk.v
@@ -16,6 +16,7 @@ module mor1kx_simple_dpram_sclk
   #(
     parameter ADDR_WIDTH = 32,
     parameter DATA_WIDTH = 32,
+    parameter CLEAR_ON_INIT = 0,
     parameter ENABLE_BYPASS = 1
     )
    (
@@ -30,6 +31,15 @@ module mor1kx_simple_dpram_sclk
 
    reg [DATA_WIDTH-1:0]     mem[(1<<ADDR_WIDTH)-1:0];
    reg [DATA_WIDTH-1:0]     rdata;
+
+generate
+if(CLEAR_ON_INIT) begin :clear_on_init
+   integer 		    idx;
+   initial
+     for(idx=0; idx < (1<<ADDR_WIDTH); idx=idx+1)
+       mem[idx] = {DATA_WIDTH{1'b0}};
+end
+endgenerate
 
 generate
 if (ENABLE_BYPASS) begin : bypass_gen
