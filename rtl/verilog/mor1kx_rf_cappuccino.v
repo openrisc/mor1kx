21f6b85fd86eb9c10f23eb2228b89b07025b4de5
Stafford Horne <shorne@gmail.com>
2017-11-26 12:01:27 +0900

    Fix iverilog compile error: hierarchical reference constant
    In iVerilog we get compile errors like:
    
      ../src/mor1kx_5.0/rtl/verilog/mor1kx_cpu_cappuccino.v:1132: error: A
        hierarchical reference (`mor1kx_rf_cappuccino.RF_ADDR_WIDTH') is not
        allowed in a constant expression.
      ../src/mor1kx_5.0/rtl/verilog/mor1kx_cpu_cappuccino.v:1132: error: Range
        expressions must be constant.
    
    Fix this by creating a function to calculate the RF_ADDR_WIDTH and use
    that instead.
    
    Tested with:
    
      fusesoc sim mor1kx-generic --elf ../../hello.newlib
    
    Reported-By: Rohit Singh <@RohitK_Singh>
    Signed-off-by: Stafford Horne <shorne@gmail.com>

--- rtl/verilog/mor1kx_rf_cappuccino.v
+++ rtl/verilog/mor1kx_rf_cappuccino.v
@@ -76,10 +76,16 @@ module mor1kx_rf_cappuccino
     );
 
 `include "mor1kx_utils.vh"
+   function integer calc_rf_addr_width;
+      input integer rf_addr_width;
+      input integer rf_num_shadow_gpr;
+      calc_rf_addr_width  = rf_addr_width +
+                           ((rf_num_shadow_gpr == 1) ? 1 :
+                             `clog2(rf_num_shadow_gpr));
+   endfunction
 
-   localparam RF_ADDR_WIDTH = OPTION_RF_ADDR_WIDTH +
-                              ((OPTION_RF_NUM_SHADOW_GPR == 1) ? 1 :
-                               `clog2(OPTION_RF_NUM_SHADOW_GPR));
+   localparam RF_ADDR_WIDTH = calc_rf_addr_width(OPTION_RF_ADDR_WIDTH,
+                                                 OPTION_RF_NUM_SHADOW_GPR);
 
    wire [OPTION_OPERAND_WIDTH-1:0]    rfa_ram_o;
    wire [OPTION_OPERAND_WIDTH-1:0]    rfb_ram_o;
