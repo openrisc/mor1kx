092dbb5cb9f26a7c00c2cb7c569c1401735ce7ae
Nancy-Chauhan <nancychn1@gmail.com>
2019-06-07 19:55:17 +0530

    Changes in .travis and .travis.yml to use parallel stages

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 apt-get update && apt-get install -y curl
 
@@ -13,26 +13,8 @@ cd $HOME/src/tools
 
 cd /src
 
-function run_test {
-    export JOB=$1
-    export SIM=$2
-    export PIPELINE=$3
-    export EXPECTED_FAILURES=$4
-    export EXTRA_CORE_ARGS=$5
+echo "Running Job $JOB $SIM $PIPELINE"
+echo "Expected failures: $EXPECTED_FAILURES"
+echo "Extra core args: $EXTRA_CORE_ARGS"
 
-    echo "Running Job $1 $2 $3"
-
-    ./.travis/run-${JOB}.sh
-}
-
-run_test verilator
-run_test or1k-tests icarus CAPPUCCINO
-run_test or1k-tests icarus CAPPUCCINO "or1k-cy"
-run_test or1k-tests icarus CAPPUCCINO "or1k-cy" "--feature_dmmu NONE"
-run_test or1k-tests icarus CAPPUCCINO "or1k-cy or1k-dsxinsn" "--feature_immu NONE"
-run_test or1k-tests icarus CAPPUCCINO "or1k-cy" "--feature_datacache NONE"
-run_test or1k-tests icarus CAPPUCCINO "or1k-cy" "--feature_instructioncache NONE"
-run_test or1k-tests icarus CAPPUCCINO "or1k-cy" "--feature_debugunit NONE"
-run_test or1k-tests icarus CAPPUCCINO "or1k-cy or1k-cmov" "--feature_cmov NONE"
-run_test or1k-tests icarus CAPPUCCINO "or1k-cy or1k-ext" "--feature_ext NONE"
-run_test or1k-tests icarus ESPRESSO 
+./.travis/run-${JOB}.sh
