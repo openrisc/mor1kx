a141af217f85852d89bfff6a3c9429806dc28520
Harshitha <59602295+Harshitha172000@users.noreply.github.com>
2021-05-29 15:05:11 +0530

    Indent changes

--- rtl/verilog/mor1kx_true_dpram_sclk.v
+++ rtl/verilog/mor1kx_true_dpram_sclk.v
@@ -57,41 +57,31 @@ module mor1kx_true_dpram_sclk
       end
    end
 
-//Formal Checking
-
-`define ASSUME assert
-`define ASSUME assume
+/*Formal Checking*/
 
 `ifdef Formal
 
-// Checking synchronous read and write operation at port A with clock_a 
+   // Checking synchronous read and write operation at port A wrt clock_a 
+   always @(posedge clk_a) begin
+      if (we_a)
+         assert ((din_a==mem[addr_a]) && (dout_a==din_a));
+      else begin
+         assume (mem[addr_a]==32'hc032);
+         //Assume some data at addr_a, check if same data is put on data port during read operation
+         assert (dout_a==32'hc032);
+      end
+   end
 
-always @(posedge clk_a) begin
-
-if (we_a)
- assert ((din_a==mem[addr_a]) && (dout_a==din_a));
- 
-else begin
- assume (mem[addr_a]==32'hc032);
- //Assume some data at addr_a, check if same data is put on data port during read operation
- assert (dout_a==32'hc032);
-
-end
-end
-
-//Checking synchronous read and write operation at port B with clock_b
-
-always @(posedge clk_b) begin
- 
-if (we_b)
- assert ((din_b==mem[addr_b]) && (dout_b==din_b)));
- 
-else begin
- assume (mem[addr_b]==32'hbb20);
- //Assume some data at addr_b, check if same data is put on data port during read operation
- assert (dout_b==32'hbb20);
-end
-end
+   //Checking synchronous read and write operation at port B wrt clock_b
+   always @(posedge clk_b) begin
+     if (we_b)
+        assert ((din_b==mem[addr_b]) && (dout_b==din_b))); 
+     else begin
+        assume (mem[addr_b]==32'hbb20);
+        //Assume some data at addr_b, check if same data is put on data port during read operation
+        assert (dout_b==32'hbb20);
+     end
+   end
 
 `endif
 
